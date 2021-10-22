b400164afec5a1c7f04f996735f8910dd1aa3920
dommentee <chuku0929@gmail.com>
2021-10-22 00:04:12 -0400

    eddting accomplished

--- controllers/art_controller.js
+++ controllers/art_controller.js
@@ -18,8 +18,14 @@ arts.post('/', (req, res) => {
     res.redirect('/arts')
   })
 })
+//update
+arts.put('/:id', (req, res) => {
+  Arts.findByIdAndUpdate(req.params.id, req.body, (error, uploadToUpdate) => {
+    res.redirect('/arts')
+  })
+})
 
-//findall
+//find all
 arts.get('/', (req, res) => {
   Arts.find({}, (error, allUploads) => {
     res.render(
@@ -54,7 +60,18 @@ arts.get('/new', (req, res) => {
   )
 })
 
-//show selected
+//edit upload
+arts.get('/:id/edit', (req, res) => {
+  Arts.findById(req.params.id, (err, uploadToUpdate) => {
+    res.render(
+      'art_edit.ejs',
+      {
+        upload: uploadToUpdate,
+        currentUser: req.session.currentUser 
+      }
+    )
+  })
+})
 
 
 
